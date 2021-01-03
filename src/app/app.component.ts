c0f7c16ab9c49fbc405d55f383fc16c7ddcbffbb
Olivia Guzman <olguva@gmail.com>
2021-01-02 21:20:16 -0600

    Completed requirement 5. It seems to work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,38 +9,39 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'Orbit-Report';
   
-  constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
- }
+ //This is the code from requirement 1.7. Left here as a reference.
+//It was to be replaced per instructions in requirement 5. 
+//   constructor() {
+//     this.sourceList = [
+//        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
+//        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
+//        new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
+//        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
+//        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
+//     ];
+//  }
+// }
 
 // Section 5-using fetch to retrieve data. Replaced contstructor code - used given code in book.
-// constructor() {
-//   this.sourceList = [];
-//   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+constructor() {
+  this.sourceList = [];
+  let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
-  // window.fetch(satellitesUrl).then(function(response) {
-  //    response.json().then(function(data) {
+  window.fetch(satellitesUrl).then(function(response) {
+     response.json().then(function(data) {
 
-  //       let fetchedSatellites = data.satellites;
+        let fetchedSatellites = data.satellites;
         // TODO: loop over satellites
-    
-    
-
+        for (let i=0; i<fetchedSatellites.length; i++){
         // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-      
-
+        let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
         // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
-  
+        this.sourceList.push(satellite);  
+        }
 
-  //    }.bind(this));
-  // }.bind(this));
-
+     }.bind(this));
+  }.bind(this));
+} 
 }
 
 
